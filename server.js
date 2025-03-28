4cb778ef018f1b2903241b2a3c359262ab449434
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-03-28 15:04:47 +0300

    updates db.json

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
+const path = require('path'); // Import path module
+const express = require('express'); // Import express
 
+const server = jsonServer.create();
 
-const server = jsonServer.create()
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use('/api', router)
+server.use('/images', express.static(path.join(__dirname, 'images'))); // Serve static files from the images directory
+server.use(middlewares);
+server.use('/api', router);
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://Dunstankiiru.github.io') // The URL you put here is for the web application that you have deployed using Github Pages
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-})
+    res.header('Access-Control-Allow-Origin', 'https://Dunstankiiru.github.io'); // The URL you put here is for the web application that you have deployed using Github Pages
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+});
 server.listen(process.env.PORT || 5000, () => {
-    console.log('JSON Server is running')
-})
+    console.log('JSON Server is running');
+});
